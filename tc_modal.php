ab9dd5f920cbc3d0c6011797631d92be338e4d34
EdwardBellen <70645265+EdwardBellen@users.noreply.github.com>
2020-09-02 21:20:36 +0800

    Add files via upload

---
+++ tc_modal.php
@@ -0,0 +1,51 @@
+
+
+<?php include('header.php'); ?>
+
+  <body id="home">
+  <br />
+  <?php  include('navbar_index.php');  ?>
+   
+  <div class="container-fluid">
+  
+ 
+  
+  
+<div class="row">
+   
+  <div class="col-md-12">
+
+	 	<!-- Modal -->
+	<div   id="tc" tabindex="-1" >
+		<div class="modal-dialog">
+			<div class="modal-content">
+			<div class="modal-header">
+			<a href="index.php" type="button" class="close"  >&times;</a>
+				<h4 class="modal-title" id="myModalLabel">Online Forum Terms and Conditions</h4>
+			</div>
+			<div class="modal-body">
+			<p>
+By using the message board(s) you agree to these terms and conditions.<br /><br />
+All messages are the opinions of the author and are not the views or opinions of Online Forum for IT Professional and Students ("FIPAS").  Please note that some of the boards may be intended for use by young people, please have regard to this.<br /><br />
+<br /><br />
+
+			</div>
+			<div class="modal-footer">
+				<a href="index.php" type="button" class="btn btn-default"  ><i class="fa fa-times"></i> Close</a>
+				
+			</div>
+			</div>
+			
+		</div>
+		</div>
+        
+  </div>
+ 
+</div>
+ 
+  </div>
+  
+ 
+  </body>
+ 
+ 
